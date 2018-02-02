fb7a49c1533e57997e02b62507ae309430f5d00d
ganesh <ganeshpapola@gmail.com>
2018-02-02 10:08:31 +0530

    express sessions are working now

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,3 @@
 export { default as usersController } from './users';
-export { default as matchPassword } from './users';
+export { default as middleware } from './middleware';
+// export { default as matchPassword } from './users';
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,4 @@
-import {usersController, matchPassword} from '../controllers';
+import {usersController, middleware} from '../controllers';
 import passport from "passport";
 import { Strategy } from "passport-local";
 
@@ -9,6 +9,7 @@ import { Strategy } from "passport-local";
 export default (server ) => {
     server.post("/login", usersController.login);
     server.post("/register",  usersController.register);
-    server.get("/test", usersController.varifyToken,  usersController.test);
+    server.post("/logout",  usersController.logOut);
+    server.get("/test", middleware.varifyToken,  usersController.test);
     
 }
